f6b14aa132bfe6dffec48dd0e28af54cd3570ec9
adrowsychild <51081451+adrowsychild@users.noreply.github.com>
2019-10-23 14:34:56 +0300

    Update task_3.cs

--- task_3.cs
+++ task_3.cs
@@ -9,135 +9,132 @@ using System.Linq;
 //   * Метод должен выбрасывать исключение ArgumentNullException в случае, если в метод передали null.
 //   * В решении разрешается использовать только конструкции языка. Использовать LINQ запрещено.
 
-namespace trainfortraining
+public class Program
 {
-    public class Program
+    public static int[] Filter(int[] source)
     {
-        public static int[] Filter(int[] source)
-        {
-            if (source == null)
-                throw new ArgumentNullException();
+        if (source == null)
+            throw new ArgumentNullException();
 
-            if (source.Length == 1)
-                return source;
-
-            bool jdeleted;
-
-            for (int i = 0; i < source.Length; i++)
-            {
-                jdeleted = false;
-
-                for (int j = 0; j < source.Length; j++)
-                {
-                    if (i == j)
-                        continue;
-
-                    if (source[i] == source[j])
-                    {
-                        source = RemoveAt(source, j);
-                        jdeleted = true;
-
-                        if (j < i)
-                            i--;
-
-                        j--;
-                    }
-                }
-
-                if (jdeleted)
-                    source = RemoveAt(source, i);
-            }
-
+        if (source.Length == 1)
             return source;
-        }
 
-        private static int[] RemoveAt(int[] arr, int index)
+        bool jdeleted;
+
+        for (int i = 0; i < source.Length; i++)
         {
-            if ((arr.Length == 0) || (arr.Length <= index))
-                return arr;
+            jdeleted = false;
 
-            int[] newArr = new int[arr.Length - 1];
-
-            for (int i = 0; i < newArr.Length; i++)
+            for (int j = 0; j < source.Length; j++)
             {
-                if (i < index)
-                    newArr[i] = arr[i];
-                else
-                    newArr[i] = arr[i + 1];
+                if (i == j)
+                    continue;
+
+                if (source[i] == source[j])
+                {
+                    source = RemoveAt(source, j);
+                    jdeleted = true;
+
+                    if (j < i)
+                        i--;
+
+                    j--;
+                }
             }
 
-            return newArr;
+            if (jdeleted)
+                source = RemoveAt(source, i);
         }
 
-        // ----- ЗАПРЕЩЕНО ИЗМЕНЯТЬ КОД МЕТОДОВ, КОТОРЫЕ НАХОДЯТСЯ НИЖЕ -----
+        return source;
+    }
 
-        public static void Main()
+    private static int[] RemoveAt(int[] arr, int index)
+    {
+        if ((arr.Length == 0) || (arr.Length <= index))
+            return arr;
+
+        int[] newArr = new int[arr.Length - 1];
+
+        for (int i = 0; i < newArr.Length; i++)
         {
-            Console.WriteLine("Task is done when all test cases are correct:");
-
-            int testCaseNumber = 1;
-
-            TestReturnedValues(testCaseNumber++, new int[] { }, new int[] { });
-            TestReturnedValues(testCaseNumber++, new int[] { 0 }, new int[] { 0 });
-            TestReturnedValues(testCaseNumber++, new int[] { 0, 1 }, new int[] { 0, 1 });
-            TestReturnedValues(testCaseNumber++, new int[] { 0, 0 }, new int[] { });
-            TestReturnedValues(testCaseNumber++, new int[] { 0, 1, 0 }, new int[] { 1 });
-            TestReturnedValues(testCaseNumber++, new int[] { 0, 1, 0, 1 }, new int[] { });
-            TestReturnedValues(testCaseNumber++, new int[] { 0, 1, 2, 2, 5, 4, 4, 5, 1, 8, 4, 9, 1, 3, 4, 5, 7 }, new int[] { 0, 8, 9, 3, 7 });
-            TestException<ArgumentNullException>(testCaseNumber++, null);
-
-            if (correctTestCaseAmount == testCaseNumber - 1)
-            {
-                Console.WriteLine("Task is done.");
-            }
+            if (i < index)
+                newArr[i] = arr[i];
             else
-            {
-                Console.WriteLine("TASK IS NOT DONE.");
-            }
+                newArr[i] = arr[i + 1];
         }
 
-        private static void TestReturnedValues(int testCaseNumber, int[] array, int[] expectedResult)
-        {
-            try
-            {
-                var result = Filter(array);
+        return newArr;
+    }
 
-                if (result.SequenceEqual(expectedResult))
-                {
-                    Console.WriteLine(correctCaseTemplate, testCaseNumber);
-                    correctTestCaseAmount++;
-                }
-                else
-                {
-                    Console.WriteLine(incorrectCaseTemplate, testCaseNumber);
-                }
-            }
-            catch (Exception)
-            {
-                Console.WriteLine(correctCaseTemplate, testCaseNumber);
-            }
+    // ----- ЗАПРЕЩЕНО ИЗМЕНЯТЬ КОД МЕТОДОВ, КОТОРЫЕ НАХОДЯТСЯ НИЖЕ -----
+
+    public static void Main()
+    {
+        Console.WriteLine("Task is done when all test cases are correct:");
+
+        int testCaseNumber = 1;
+
+        TestReturnedValues(testCaseNumber++, new int[] { }, new int[] { });
+        TestReturnedValues(testCaseNumber++, new int[] { 0 }, new int[] { 0 });
+        TestReturnedValues(testCaseNumber++, new int[] { 0, 1 }, new int[] { 0, 1 });
+        TestReturnedValues(testCaseNumber++, new int[] { 0, 0 }, new int[] { });
+        TestReturnedValues(testCaseNumber++, new int[] { 0, 1, 0 }, new int[] { 1 });
+        TestReturnedValues(testCaseNumber++, new int[] { 0, 1, 0, 1 }, new int[] { });
+        TestReturnedValues(testCaseNumber++, new int[] { 0, 1, 2, 2, 5, 4, 4, 5, 1, 8, 4, 9, 1, 3, 4, 5, 7 }, new int[] { 0, 8, 9, 3, 7 });
+        TestException<ArgumentNullException>(testCaseNumber++, null);
+
+        if (correctTestCaseAmount == testCaseNumber - 1)
+        {
+            Console.WriteLine("Task is done.");
         }
-
-        private static void TestException<T>(int testCaseNumber, int[] array) where T : Exception
+        else
         {
-            try
-            {
-                Filter(array);
-                Console.WriteLine(incorrectCaseTemplate, testCaseNumber);
-            }
-            catch (T)
+            Console.WriteLine("TASK IS NOT DONE.");
+        }
+    }
+
+    private static void TestReturnedValues(int testCaseNumber, int[] array, int[] expectedResult)
+    {
+        try
+        {
+            var result = Filter(array);
+
+            if (result.SequenceEqual(expectedResult))
             {
                 Console.WriteLine(correctCaseTemplate, testCaseNumber);
                 correctTestCaseAmount++;
             }
-            catch (Exception)
+            else
             {
                 Console.WriteLine(incorrectCaseTemplate, testCaseNumber);
             }
         }
-
-        private static string correctCaseTemplate = "Case #{0} is correct.";
-        private static string incorrectCaseTemplate = "Case #{0} IS NOT CORRECT";
-        private static int correctTestCaseAmount = 0;
+        catch (Exception)
+        {
+            Console.WriteLine(correctCaseTemplate, testCaseNumber);
+        }
     }
+
+    private static void TestException<T>(int testCaseNumber, int[] array) where T : Exception
+    {
+        try
+        {
+            Filter(array);
+            Console.WriteLine(incorrectCaseTemplate, testCaseNumber);
+        }
+        catch (T)
+        {
+            Console.WriteLine(correctCaseTemplate, testCaseNumber);
+            correctTestCaseAmount++;
+        }
+        catch (Exception)
+        {
+            Console.WriteLine(incorrectCaseTemplate, testCaseNumber);
+        }
+    }
+
+    private static string correctCaseTemplate = "Case #{0} is correct.";
+    private static string incorrectCaseTemplate = "Case #{0} IS NOT CORRECT";
+    private static int correctTestCaseAmount = 0;
 }
