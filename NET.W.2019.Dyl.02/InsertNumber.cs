59ce3ad1f6d19a8412e192e4a2d477f80a19a569
adrowsychild <51081451+adrowsychild@users.noreply.github.com>
2019-11-17 21:23:19 +0300

    Add files via upload

---
+++ NET.W.2019.Dyl.02/InsertNumber.cs
@@ -0,0 +1,43 @@
+using System;
+
+namespace NET.W._2019.Dyl._02
+{
+    class Program
+    {
+		/// <summary>
+        /// A method that inserts bits (from j to i) from one number to another
+        /// so that bits of the second number took the positions from j to i
+        /// </summary>
+        /// <param name="numSourse"></param>
+        /// <param name="numIn"></param>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        /// <returns>Int32</returns>
+        public static int InsertNumber(int numSourse, int numIn, int i, int j)
+        {
+            int result = 0;
+            int mask = 0;
+            int numInShifted = numIn << i;
+
+            if (i == j)
+                mask = setOne(mask, i);
+            else
+            {
+                for (int k = i; k <= j; k++)
+                    mask = setOne(mask, k);
+            }
+
+            result = (mask & numInShifted) | (~mask & numSourse);
+
+            return result;
+    }
+
+        private static int setOne(int numSourse, int i)
+        {
+            int tmp = 1 << i;
+            numSourse = numSourse | tmp;
+
+            return numSourse;
+        }
+	}
+}
