8ab40bd952907de7b9e702aadaedd5ea667cef7e
adrowsychild <51081451+adrowsychild@users.noreply.github.com>
2019-11-21 19:42:55 +0300

    Add files via upload

---
+++ NET.W.2019.Dyl.04/Extension.cs
@@ -0,0 +1,51 @@
+﻿//-------------------------
+// <copyright file="Extension.cs" company="EPAM">
+//     .NET training
+// </copyright>
+//-------------------------
+
+namespace NET.W._2019.Dyl._04
+{
+    using System;
+    using System.Collections;
+    using System.Text;
+
+    /// <summary>
+    /// A class for extension
+    /// </summary>
+    public static class Extension
+    {
+        /// <summary>
+        /// Implementing binary conversion using bit converter
+        /// </summary>
+        /// <param name="db">Double value</param>
+        /// <returns>String in binary format</returns>
+        public static string DoubleToBinaryString(this double db)
+        {
+            BitArray bitArray = new BitArray(BitConverter.GetBytes(db));
+            StringBuilder res = new StringBuilder(64);
+
+            for (int i = bitArray.Length - 1; i >= 0; i--)
+            {
+                if (bitArray[i] == false)
+                {
+                    res.Append('0');
+                }
+                else
+                {
+                    res.Append('1');
+                }
+            }
+
+            return res.ToString();
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+}
