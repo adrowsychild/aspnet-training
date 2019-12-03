7e98dd5b5148578eefed1e46c7167b3f53cd925a
adrowsychild <a.drowsy.child@gmail.com>
2019-12-03 23:15:52 +0300

    NET.W.2019.Dyl.10 task4 completed

---
+++ NET.W.2019.Dyl.10/EuqlidRefactor/Program.cs
@@ -0,0 +1,15 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="EPAM">
+//     .NET training
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace EuqlidRefactor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
