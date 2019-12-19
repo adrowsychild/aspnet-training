eba3bda502f8f33d8ff3454df563f6e98d42046e
adrowsychild <a.drowsy.child@gmail.com>
2019-12-19 12:37:16 +0300

    readme added.

--- NET.W.2019.Dyl.13/BinarySearchTree/Program.cs
+++ NET.W.2019.Dyl.13/BinarySearchTree/Program.cs
@@ -7,28 +7,6 @@ namespace BinarySearchTree
     {
         static void Main(string[] args)
         {
-            BinarySearchTree<string> stringTree = new BinarySearchTree<string>();
-            stringTree.Insert("qwerty");
-            stringTree.Insert("ewq");
-            stringTree.Insert("weq");
-            stringTree.Insert("ytr");
-            stringTree.Insert("rew");
-            stringTree.Insert("ewq");
-            stringTree.Insert("werty");
-            stringTree.Insert("ert");
-
-            List<string> strings = new List<string>();
-            foreach (var n in stringTree.PreorderTraversal())
-            {
-                strings.Add(n);
-            }
-
-            foreach (var s in stringTree)
-            {
-                Console.WriteLine(s);
-            }
-
-            Console.ReadKey();
         }
     }
 }
